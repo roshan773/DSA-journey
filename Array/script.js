a11361c93006b890a2ce1f580c6dba2680b35463
Pakhre Roshan <pakhreroshan@gmail.com>
2025-08-23 23:43:37 +0530

    Done question of array with build in methods

---
+++ Array/script.js
@@ -0,0 +1,39 @@
+// Array Build in methods
+
+// Push: Push method is used for adding any element in the last index of array
+// let numbers = [10, 20, 30, 40, 50, 60, 70];
+// numbers.push(80)
+// console.log(numbers)
+
+// Pop: Pop build in method is used for removing last index of element from array
+// let numbers = [10, 20, 30, 40, 50, 60, 70]
+// numbers.pop()
+// console.log(numbers)
+
+// Unshift: Unshift build in method is  used for adding element in first index of array
+// let numbers = [10, 20, 30, 40, 50, 60, 70]
+// numbers.unshift(0)
+// console.log(numbers)
+
+// Shift: Shift build in method is used for removing first index of element in the array
+// let numbers = [10, 20, 30, 40, 50, 60, 70, 80]
+// numbers.shift()
+// console.log(numbers)
+
+// Splice: Splice is used for adding, removing or replacing of element from any index of element
+// let numbers = [10, 20, 40, 50, 60]
+// console.log(numbers)
+// numbers.splice(1, 0, 30)
+// console.log(numbers)
+
+
+// cocate: Concate is used to join two array
+// let numbers = [10, 20, 40, 50, 60]
+// console.log(numbers.concat([70, 80, 90]))
+
+
+// Indexof: Indexof is used to find the index of any array
+// let numbers = [10, 20, 40, 50, 60, 70, 80, 90]
+// console.log(numbers.indexOf(90))
+
+// Map: 
