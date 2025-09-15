511226f886f1483b8073d6f670cee1153634b967
Pakhre Roshan <pakhreroshan@gmail.com>
2025-09-15 13:24:42 +0530

    Done wirg optimize code

--- Sorting/Bubble sort/bubble_sort_asc.js
+++ Sorting/Bubble sort/bubble_sort_asc.js
@@ -1,12 +1,15 @@
 let arr = [20, 10, 43, 3, 6, 20, 33, 34, 78]
-let n = arr.length
+// let n = arr.length
 
-for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - 1; j++) {
-        if (arr[i] < arr[j - 1]) {
-            [arr[i], arr[j - 1]] = [arr[j - 1], arr[i]]
-        }
-    }
-}
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length - 1; j++) {
+//         if (arr[i] < arr[j - 1]) {
+//             [arr[i], arr[j - 1]] = [arr[j - 1], arr[i]]
+//         }
+//     }
+// }
 
+// console.log(arr)
+
+arr.sort((a,b) => a-b)
 console.log(arr)
