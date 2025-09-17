da41c62ac884f3ad4cdee94a79df11788feb4f90
Pakhre Roshan <pakhreroshan@gmail.com>
2025-09-17 11:15:46 +0530

    Practice of sorting algorithm

--- Practice/Bubble sort/des_sort.js
+++ Practice/Bubble sort/des_sort.js
@@ -1,7 +1,7 @@
 let arr = [2, 3, 73, 28, 92, 6, 90]
 
 for (i = 0; i < arr.length; i++) {
-    for (j = 0; j < arr.length - 1; j++) {
+    for (j = 0; j < arr.length - i - 1; j++) {
         if (arr[j] < arr[j + 1]) {
             [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
         }
